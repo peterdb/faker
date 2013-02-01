3b08802429075b30f75f24f8c4081b8af5af70df
Peter De Bruycker <peter.de.bruycker@gmail.com>
2013-02-01 15:50:57 +0100

    removed obsolete asType method

--- src/test/easyb/support/DateUtils.groovy
+++ src/test/easyb/support/DateUtils.groovy
@@ -7,14 +7,6 @@ import org.codehaus.groovy.runtime.DefaultGroovyMethods
 
 class DateUtils {
 
-    static <T> T asType(String self, Class<T> type) {
-        if(Date == type) {
-            return new Date().parse("dd/MM/yyyy", self)
-        }
-        
-        DefaultGroovyMethods.asType(self, type)
-    }
-    
     static <T> T asType(List self, Class<T> type) {
         if(Date == type && self.size() == 3) {
             Calendar cal = Calendar.getInstance()
